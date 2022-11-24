a4e9c6853f3e552de01339da7ab2c23efe9d5e20
Andreas Lillje <al225wg@student.lnu.se>
2022-11-24 09:48:38 +0100

    Add layout component

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,20 @@
+import './layout.css'
+import * as React from 'react'
+
+/**
+ * Layout Component.
+ * Sets the page layout with CSS grid and inserts the children into the main HTML div element.
+ *
+ * @param {React.ReactElement} children - The React Element to insert into the component.
+ * @returns {React.ReactElement} - Layout Component.
+ */
+const Layout = ({ children }) => {
+
+  return (
+    <div className="layoutContainer">
+
+    </div>
+  )
+}
+
+export default Layout
