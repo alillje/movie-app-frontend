f99c9d9829b780c3fe4a61a549af10c08655f5d8
Andreas Lillje <al225wg@student.lnu.se>
2022-11-25 10:22:33 +0100

    Fix onChange in SearchFiled-component

--- src/components/search-field/search-field.js
+++ src/components/search-field/search-field.js
@@ -1,15 +1,53 @@
 import './search-field.css'
 import * as React from 'react'
+import { useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { setSearch } from '../../redux/reducers/search'
 
 /**
  * SearchField Component.
  *
- * @param {React.ReactElement} children - The React Element to insert into the component.
  * @returns {React.ReactElement} - SearchField Component.
  */
 const SearchField = () => {
+  const [currentSearch, setCurrentSearch] = useState('')
+  const dispatch = useDispatch()
+
+  /**
+   *
+   */
+  const setSearchPhrase = () => {
+    dispatch(
+      setSearch({
+        isSearching: true,
+        searchPhrase: currentSearch
+      })
+    )
+  }
+
+  /**
+   *
+   *
+   */
+  const clearSearchPhrase = () => {
+    dispatch(
+      setSearch({
+        isSearching: false,
+        searchPhrase: null
+      })
+    )
+  }
+
+  /**
+   *
+   *
+   * @returns {*}
+   */
+  useEffect(() => {
+    currentSearch.length ? setSearchPhrase() : clearSearchPhrase({})
+  }, [currentSearch])
   return (
-    <input type="text" placeholder="Search Movie Titles" className="searchfieldInput">
+    <input value={currentSearch} onChange={(event) => setCurrentSearch(event.target.value)} type="text" placeholder="Search Movie Titles" className="searchfieldInput">
     </input>
   )
 }
